d155b63913add6e002f8861c37df2de5a7c1e2e4
Francesco Ivone <francesco.ivone@cern.ch>
2021-04-25 10:05:31 +0200

    Updated muDpgNtuples_cfg to run over MWGR data

--- test/muDpgNtuples_cfg.py
+++ test/muDpgNtuples_cfg.py
@@ -9,11 +9,7 @@ import sys
 options = VarParsing.VarParsing()
 
 options.register('globalTag',
-                 #'111X_dataRun3_Express_v4',
-                 #"110X_mcRun3_2021_realistic_v9",
-                 '112X_mcRun3_2021_realistic_v10',
-                 #'auto:phase1_2021_realistic',
-                 #'111X_upgrade2018_realistic_v1', #default value
+                 '112X_dataRun3_Express_v5',
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
                  "Global Tag")
@@ -34,22 +30,14 @@ options.register('isMC',
 
 
 options.register('inputFolder',
-                 "/lustre/cms/store/user/gmilella/Run3Summer19GS-step0/CRAB3_MC_ZMuMu_RECO/201111_170703/0000/",
+                 #"/lustre/cms/store/user/gmilella/Run3Summer19GS-step0/CRAB3_MC_ZMuMu_RECO/201111_170703/0000/",
                  #"/eos/user/f/fivone/GEMNTuples/MWGR/338714/0000/",
+                 "/eos/cms//store/express/Commissioning2021/ExpressCosmics/FEVT/Express-v1/000/341/112/00000/",
                  #"/eos/cms//store/express/Commissioning2020/ExpressCosmics/FEVT/Express-v1/000/338/714/00000/",
                  #"/eos/user/f/fivone/GEMNTuples/MC/Input/",
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
                  "EOS folder with input files")
-options.register('inputFile',
-                 "",
-                 #"/eos/user/f/fivone/GEMNTuples/MWGR/338714/0000/",
-                 #"/eos/cms//store/express/Commissioning2020/ExpressCosmics/FEVT/Express-v1/000/338/714/00000/",
-                 #"/eos/user/f/fivone/GEMNTuples/MC/Input/",
-                 VarParsing.VarParsing.multiplicity.singleton,
-                 VarParsing.VarParsing.varType.string,
-                 "File name")
-
 
 options.register('secondaryInputFolder',
                  '', #default value
@@ -59,7 +47,7 @@ options.register('secondaryInputFolder',
 
 options.register('ntupleName',
                  #'./MuDPGNtuple_11_1_2_patch2.root',
-                 "GEMNtuples.root",
+                 "MuDPGNTuple_MWGR3_EXP_run341343.root",
                  #'/lustre/cms/store/user/gmilella/Cosmics/CONDOR_gem_dpg_ntuple_mwgr4_run337973_eos/MuDPGNtuple_MWGR4eos.root',
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
@@ -94,12 +82,11 @@ process.source = cms.Source("PoolSource",
 if "eos/cms" in options.inputFolder:
     files = subprocess.check_output(['ls', options.inputFolder])
     files = files.split()
-    process.source.fileNames = ["file:"+options.inputFolder+f for f in files]
+    process.source.fileNames = ["file:"+options.inputFolder+ "/" + f for f in files]
 
 elif "lustre/cms/store" in options.inputFolder:
     files = subprocess.check_output(['ls', options.inputFolder])
     files = files.split()
-    files = [ f for f in files if options.inputFile in f]
     print files
     process.source.fileNames = ["file://" + options.inputFolder + "/" + f for f in files]
 
@@ -118,7 +105,7 @@ if options.secondaryInputFolder != "" :
  
 
 process.TFileService = cms.Service('TFileService',
-                                   fileName = cms.string("GEMntuples_"+options.inputFile)
+                                   fileName = cms.string(options.ntupleName)
     )
 
 
